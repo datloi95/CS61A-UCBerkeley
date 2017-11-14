6786c2078f50122ff4860de55ecabf85e1b8cdda
Dat Loi <datloi95@gmail.com>
2017-11-14 16:30:37 -0500

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,111 @@
+
+
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = a - b
+    else:
+        f = a + b
+    return f
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    "*** YOUR CODE HERE ***"
+    a =[a,b,c]
+    a.sort()
+    print(a)
+    return a[2]*a[2] + a[1]*a[1]
+
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+def with_if_statement():
+    """
+    >>> with_if_statement()
+    1
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+def c():
+    "*** YOUR CODE HERE ***"
+
+def t():
+    "*** YOUR CODE HERE ***"
+
+def f():
+    "*** YOUR CODE HERE ***"
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+	
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    sequence = [n]
+    if n == 1
+    	return a
+    else if n%2 == 0
+    	sequence.append(n/2)
+    	return hailstone(n/2)
+    else if n%2 != 0
+    	sequence.append((3 * n) + 1)
+    	return hailstone((3 * n) + 1)
+
+
+
+challenge_question_program = """
+"*** YOUR CODE HERE ***"
+"""
+
+
+
